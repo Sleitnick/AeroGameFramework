7ffcf28ce828f57c4fb32eeff90dba90ac9d4299
Stephen <sleitnick@gmail.com>
2017-07-23 11:54:47 -0400

    Filelist builder update

--- build_filelist.py
+++ build_filelist.py
@@ -11,27 +11,46 @@ import json
 filelistName = "filelist.json"
 
 prefix = "https://raw.githubusercontent.com/Sleitnick/AeroGameFramework/master/src/"
+emptyDirSuffix = "EMPTY"
 
 rootdir = os.path.join(".", "src")
 data = {}
 data["url"] = prefix
 data["paths"] = []
 
+
+def fix_path(path):
+	return path[len(rootdir) + 1:].replace("\\", "/")
+
+
 # Get all files:
-for subdir,dirs,files in os.walk(rootdir):
-	if (len(files) == 0 and subdir != rootdir and len(dirs) == 0):
-		data["paths"].append(subdir[len(rootdir) + 1:].replace("\\", "/") + "/EMPTY")
-	for file in files:
-		path = os.path.join(subdir, file)[len(rootdir) + 1:].replace("\\", "/")
-		data["paths"].append(path)
+def get_all_files():
+	for subdir,dirs,files in os.walk(rootdir):
+		if (len(files) == 0 and subdir != rootdir and len(dirs) == 0):
+			#data["paths"].append(subdir[len(rootdir) + 1:].replace("\\", "/") + "/" + emptyDirSuffix)
+			data["paths"].append(fix_path(rootdir) + "/" + emptyDirSuffix)
+		for file in files:
+			#path = os.path.join(subdir, file)[len(rootdir) + 1:].replace("\\", "/")
+			path = fix_path(os.path.join(subdir, file))
+			data["paths"].append(path)
 
-# Sort and concatenate array:
-data["paths"].sort(key=len)
-filelistStr = json.dumps(data, indent=4)
 
-# Write file:
-filelist = open(filelistName, "w")
-filelist.write(filelistStr)
-filelist.close()
+def write_to_filelist_file():
 
+	# Sort and concatenate array:
+	data["paths"].sort(key=len)
+	filelistStr = json.dumps(data, indent=4)
+
+	# Write file:
+	filelist = open(filelistName, "w")
+	filelist.write(filelistStr)
+	filelist.close()
+
+
+def build():
+	get_all_files()
+	write_to_filelist_file()
+
+
+build()
 print "File list built"
