b8a2bdcafee53e903ad48e12a8900b5e53cb1af3
Stephen Leitnick <sleitnick@gmail.com>
2019-10-17 15:33:08 -0400

    Make function local

--- src/ReplicatedStorage/Aero/Shared/Date.lua
+++ src/ReplicatedStorage/Aero/Shared/Date.lua
@@ -89,7 +89,7 @@ local MONTHS_SHORT = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "S
 
 
 -- Single-level table copy:
-function CopyTable(t)
+local function CopyTable(t)
 	local tCopy = {}
 	for k,v in pairs(t) do
 		tCopy[k] = v
